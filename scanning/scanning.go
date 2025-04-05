df0f9aee65cb9ed8cf5eef0fbdf1a5c6c5df9043
Karidus-423 <kapuerto23@gmail.com>
2025-04-05 00:41:30 +0000

    Give image, return matrix

--- scanning/scanning.go
+++ scanning/scanning.go
@@ -1,5 +1,37 @@
 package scanning
 
-func ScanImage() {
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	_ "image/png"
+	"os"
+)
 
+func DecodeImage(filename string) (image.Image, string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, "", err
+	}
+
+	defer file.Close()
+
+	img, format, err := image.Decode(file)
+	if err != nil {
+		return nil, "", err
+	}
+	return img, format, nil
+}
+
+func GetImageMatrix(pic image.Image) (grid [][]color.Color) {
+	size := pic.Bounds()
+	for i := range size.Max.X {
+		var y []color.Color
+		for j := range size.Max.Y {
+			y = append(y, pic.At(i, j))
+		}
+		grid = append(grid, y)
+	}
+	return grid
 }
